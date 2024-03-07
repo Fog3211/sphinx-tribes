c373eddec1bca18f3efca5a15457105d39e16596
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2024-03-08 02:01:51 +0800

    test: add test for access

--- db/config_test.go
+++ db/config_test.go
@@ -13,16 +13,16 @@ type MockDB struct {
 	database
 }
 
-// Implementing mock methods for GetUserRoles and GetOrganizationByUuid
-func (m *MockDB) GetUserRoles(uuid string, pubKeyFromAuth string) []UserRoles {
-	args := m.Called(uuid, pubKeyFromAuth)
-	return args.Get(0).([]UserRoles)
-}
+// // Implementing mock methods for GetUserRoles and GetOrganizationByUuid
+// func (m *MockDB) GetUserRoles(uuid string, pubKeyFromAuth string) []UserRoles {
+// 	args := m.Called(uuid, pubKeyFromAuth)
+// 	return args.Get(0).([]UserRoles)
+// }
 
-func (m *MockDB) GetOrganizationByUuid(uuid string) Organization {
-	args := m.Called(uuid)
-	return args.Get(0).(Organization)
-}
+// func (m *MockDB) GetOrganizationByUuid(uuid string) Organization {
+// 	args := m.Called(uuid)
+// 	return args.Get(0).(Organization)
+// }
 
 func TestRolesCheck(t *testing.T) {
 	userRoles := []UserRoles{
@@ -44,4 +44,45 @@ func TestCheckUser(t *testing.T) {
 	// if in the user roles, one of the owner_pubkey belongs to the user return true else return false
 	assert.True(t, CheckUser(userRoles, "userPublicKey"))
 	assert.False(t, CheckUser(userRoles, "anotherPublicKey"))
+}
+
+func TestUserHasAccess(t *testing.T) {
+	mockDB := new(MockDB)
+
+	org := Organization{OwnerPubKey: "adminKey"}
+	mockDB.On("GetOrganizationByUuid", "orgUUID").Return(org)
+
+	// Test for admin access
+	assert.True(t, DB.UserHasAccess("adminKey", "orgUUID", "ANY ROLE"))
+
+	// Test for non-admin with role access
+	userRoles := []UserRoles{{Role: "VIEW REPORT"}}
+	mockDB.On("GetUserRoles", "orgUUID", "userKey").Return(userRoles)
+	assert.True(t, DB.UserHasAccess("userKey", "orgUUID", "VIEW REPORT"))
+
+	// Test for non-admin without role access
+	assert.False(t, DB.UserHasAccess("userKey", "orgUUID", "ADD BOUNTY"))
+}
+
+func TestUserHasManageBountyRoles(t *testing.T) {
+	mockDB := new(MockDB)
+
+	org := Organization{OwnerPubKey: "adminKey"}
+	mockDB.On("GetOrganizationByUuid", "orgUUID").Return(org)
+
+	// Test for organization admin
+	assert.True(t, DB.UserHasManageBountyRoles("adminKey", "orgUUID"))
+
+	// Test for user with all manage bounty roles
+	userRoles := make([]UserRoles, len(ManageBountiesGroup))
+	for i, role := range ManageBountiesGroup {
+		userRoles[i] = UserRoles{Role: role}
+	}
+	mockDB.On("GetUserRoles", "orgUUID", "userKey").Return(userRoles)
+	assert.True(t, DB.UserHasManageBountyRoles("userKey", "orgUUID"))
+
+	// Test for user missing some manage bounty roles
+	partialRoles := userRoles[:len(userRoles)-1]
+	mockDB.On("GetUserRoles", "orgUUID", "userKey").Return(partialRoles)
+	assert.False(t, DB.UserHasManageBountyRoles("userKey", "orgUUID"))
 }
