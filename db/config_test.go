b21cac75ae00187d53740da57826128c648edce4
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2024-03-08 00:01:40 +0800

    test: add unit test for role check

---
+++ db/config_test.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// Mocking the database structure for testing
+type MockDB struct {
+	mock.Mock
+	database
+}
+
+// Implementing mock methods for GetUserRoles and GetOrganizationByUuid
+func (m *MockDB) GetUserRoles(uuid string, pubKeyFromAuth string) []UserRoles {
+	args := m.Called(uuid, pubKeyFromAuth)
+	return args.Get(0).([]UserRoles)
+}
+
+func (m *MockDB) GetOrganizationByUuid(uuid string) Organization {
+	args := m.Called(uuid)
+	return args.Get(0).(Organization)
+}
+
+func TestRolesCheck(t *testing.T) {
+	userRoles := []UserRoles{
+		{Role: "ADD BOUNTY"},
+	}
+
+	// test returns true when a user has a role
+	assert.True(t, RolesCheck(userRoles, "ADD BOUNTY"))
+
+	// returns false when a use does not have a role
+	assert.False(t, RolesCheck(userRoles, "DELETE BOUNTY"))
+	assert.False(t, RolesCheck(userRoles, "DELETE BOUNTY2"))
+}
+
+func TestCheckUser(t *testing.T) {
+	userRoles := []UserRoles{
+		{OwnerPubKey: "userPublicKey"},
+	}
+	// if in the user roles, one of the owner_pubkey belongs to the user return true else return false
+	assert.True(t, CheckUser(userRoles, "userPublicKey"))
+	assert.False(t, CheckUser(userRoles, "anotherPublicKey"))
+}
